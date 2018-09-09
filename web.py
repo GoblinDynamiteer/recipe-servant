e8bef71ebffd27cde9f9b2ba17b30f46a64d8182
Johan Kämpe <kampe.johan@gmail.com>
2018-09-09 21:16:36 +0200

    web: use recipe module to generate and display a sample recipe
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

--- web.py
+++ web.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3.6
 
+''' Serves a CherryPy recipe site '''
+
 import cherrypy
+import recipe
 
 LAN_IP = '192.168.0.193'
 
+
+
 class HelloWorld(object):
     ''' CherryPy HelloWorld '''
-    
+    def __init__(self, recipes):
+        self.recipes = recipes
     def index(self):
         ''' Index path'''
-        return "Hello World!"
+        return self.recipe_to_html(self.recipes[0])
 
     index.exposed = True
 
+    def recipe_to_html(self, recipe_to_convert):
+        title = recipe_to_convert.title
+        html = f"<h1>{title}</h1><br>\n"
+        html += f"Placeholder text"
+        return html
+
+RECIPES = []
+REC = recipe.Recipe("Pasta med tonfisksås och sojabönor", 4, 15)
+
+RECIPES.append(REC)
+
 cherrypy.config.update({'server.socket_host': LAN_IP})
-cherrypy.quickstart(HelloWorld())
+cherrypy.quickstart(HelloWorld(RECIPES))
