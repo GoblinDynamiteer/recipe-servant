819ac44499cb57bf4d34245b9100f21d3ff87ced
Johan Kämpe <kampe.johan@gmail.com>
2018-09-09 21:15:54 +0200

    recipe: add recipe module
    Has recipe class
    
    Signed-off-by: Johan Kämpe <kampe.johan@gmail.com>

---
+++ recipe.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.6
+
+'''Recipe handling'''
+
+from collections import namedtuple
+RecipeStep = namedtuple('recipe_step', 'number description')
+Ingredient = namedtuple('ingredient', 'name amount unit')
+
+UNITS = ["st", "g", "ml", "dl", "port"]
+
+class Recipe:
+    ''' Holds a recipe '''
+    def __init__(self, title, portions=None, time=None, description=None):
+        self.title = title
+        self.portions = portions
+        self.time = time
+        self.description = description
+        self.steps = []
+        self.ingredients = []
+
+    def add_instruction_step(self, description, number=None):
+        ''' Add a recipe description step '''
+        number = number if number is not None else len(self.steps) + 1
+        self.steps.append(RecipeStep(number, description))
+
+    def add_ingredient(self, ingredient_name, amount=None, unit=None):
+        ''' Add an ingredient '''
+        if unit and unit not in UNITS:
+            print(f"wrong unit, valid: {UNITS}")
+            return
+        self.ingredients.append(Ingredient(ingredient_name, amount, unit))
+
